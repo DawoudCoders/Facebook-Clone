21b3fad63b3b216b99e70e778ca00637f2c8b8b5
dawoudcoders <dawoudtanveer@outlook.com>
2022-03-05 17:07:57 +0000

    Completed the middle section of the header w styling

--- Components/Header.js
+++ Components/Header.js
@@ -13,10 +13,11 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import { HeaderIcon } from "./HeaderIcon";
 
 export const Header = () => {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/*  header left */}
       <div className="flex items-center">
         <Image
@@ -28,15 +29,21 @@ export const Header = () => {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook"
           />
         </div>
       </div>
       {/* // header center  */}
-      <div className="flex justify-center flex-grow-0">
-        <div></div>
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
       </div>
 
       {/* // header right Header */}
