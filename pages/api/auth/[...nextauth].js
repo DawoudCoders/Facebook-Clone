3047bc40c065f176fa394b055b4fa298f8ac6a6a
dawoudcoders <dawoudtanveer@outlook.com>
2022-04-05 17:21:32 +0100

    Added callbackURL to next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,6 +7,7 @@ export default NextAuth({
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      "callbackURL": "/auth/facebook/callback/"
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
