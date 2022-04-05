72cc7eb1bbee7e015dc648bd5ce7c12174503dda
dawoudcoders <dawoudtanveer@outlook.com>
2022-04-05 15:58:24 +0100

    Added nextauth_url to env variable

--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,27 @@ import { Header } from "../Components/Header";
 import { Login } from "../Components/Login";
 import { Sidebar } from "../Components/Sidebar";
 import { Feed } from "../Components/Feed";
-import  Widgets from "../Components/Widgets"
+import Widgets from "../Components/Widgets";
 
 export default function Home({ session }) {
+  if (!session) {
+    return <Login />;
+  } else {
+    return (
+      <div className="h-screen bg-gray-100 overflow-hidden">
+        <Head>
+          <title>Facebook</title>
+        </Head>
 
-  if (!session) return <Login />;
-  else{
-  return (
-    <div className="h-screen bg-gray-100 overflow-hidden">
-      <Head>
-        <title>Facebook</title>
-      </Head>
-
-      <Header />
-      <main className="flex">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      
-      </main>
-    </div>
-  );
-}
+        <Header />
+        <main className="flex">
+          <Sidebar />
+          <Feed />
+          <Widgets />
+        </main>
+      </div>
+    );
+  }
 }
 //context comes from the request when the user comes to the page
 export async function getServerSideProps(context) {
